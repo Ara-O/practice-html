3e73b155404026ff77fae84b41bbcfcb58298515
Ara-O <oladipoeyiara@gmail.com>
2023-11-21 16:41:21 -0500

    push

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ app.get("/", (req, res) => {
     res.send("Hi everyone")
 })
 
+app.post("/signup", (req, res) => {
+    console.log(req.body)
+    res.status(200).send({ message: "Success" })
+})
+
 app.listen("8080")
